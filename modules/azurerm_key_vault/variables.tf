8df5d0e031b8787dd45e5099b6369c7ae8569eb9
sarvin901 <59525101+sarvin901@users.noreply.github.com>
2025-12-06 21:05:21 +0530

    Update variables.tf
    tenant id

--- modules/azurerm_key_vault/variables.tf
+++ modules/azurerm_key_vault/variables.tf
@@ -1,11 +1,10 @@
-variable "key_vaults" {
-  type = map(object({
-    resource_group_name = string
-    location            = string
-    sku_name           = string
-    tenant_id          = string
-    soft_delete_retention_days = number
-    purge_protection_enabled   = bool
-    enable_rbac_authorization  = bool
-  }))
+key_vaults = {
+  kv1 = {
+    resource_group_name        = "rg-demo"
+    sku_name                   = "standard"
+    tenant_id                  = "ae5039ee-38bc-4c65-9a13-5cf76c9ae444"
+    soft_delete_retention_days = 7
+    purge_protection_enabled   = true
+    enable_rbac_authorization  = true
+  }
 }
